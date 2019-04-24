9e7115d7cf92ec76efdc82aae0c908f9b9633629
Mushrifah <mushrifahhasan@gmail.com>
2019-04-25 01:52:03 +0530

    label color changed

--- static/js/main.js
+++ static/js/main.js
@@ -12,27 +12,8 @@ $("#submit").click(function(){
             //var result = JSON.parse(response);
             console.log(response);
             var result = response;
- 			$('#result').css('display', 'inline');
+ 			$('#result').css({"display": "inline","color":"black"});
  			$('#result').text(result.label);
-
-            //     $('#rfc').text("RFC: " + result.rfc);
-            // result = response
-            // if(result.status){
-            //     $('#loader_sensor').css('display', 'none');
-
-            //     $('#rfc').css('display', 'inline');
-            //     $('#kmeans').css('display', 'inline');
-            //     $('#svm').css('display', 'inline');
-
-            //     $('#rfc').text("RFC: " + result.rfc);
-            //     $('#kmeans').text("KMeans: " + result.kmeans);
-            //     $('#svm').text("SVM: " + result.svm);
-            // }else{
-            //     $('#loader_sensor').css('display', 'none');
-            //     $('#kmeans').css('display', 'inline');
-            //     $('#kmeans').text(result.message);
-            //     $('#kmeans').css('color', 'red');
-            // }
         },
     });
 
